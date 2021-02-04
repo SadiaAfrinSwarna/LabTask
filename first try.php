f8c8d87c2c7f5f708ec27539fb8297fb8ec4657f
unknown <sadiafrinswarna@gmail.com>
2021-02-04 20:08:13 +0600

    First commit

---
+++ first try.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MY WEB Tecnonology</title>
+</head>
+
+<body>
+<h1> my header </h1>
+
+<p> my para </p>
+
+<a href="https://www.aiub.edu">AIUB</a>
+
+<br><br>
+<form>
+
+First Name :
+<input type= "text" id= "f" name="fname" placeholder="Enter">
+Middle Name :
+<input type= "text" id= "l" name="lname">
+last Name :
+<input type= "text" id= "m" name="mname">
+Email :
+<input type= "text" id= "v" name="vname">
+
+</form>
+</body>
+</html>
